397e1bdea639f6bbcafd0ffaca921ff45f0a0323
morgan <morgan@maymancbv.com>
2026-01-27 13:12:01 +0000

    Initial commit with dark mode

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // Add JS here
+
+const themeToggle = document.querySelector('#checkbox');
+
+const currentTheme = localStorage.getItem('theme');
+const doc = document.documentElement;
+
+if (currentTheme) {
+    doc.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        themeToggle.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        doc.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        doc.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+themeToggle.addEventListener('change', switchTheme, false);
